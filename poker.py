170184355be9f8b70103dda1eef7ef5da2c6249c
pavanred <pavankumar.kh@gmail.com>
2014-05-19 01:06:59 -0500

    hand type, TODO tie breaks

--- poker.py
+++ poker.py
@@ -2,13 +2,69 @@
 def main():
 	card_test()
 	hand_test()
+	poker_test()
 
 class Poker(object):
 	def __init__(self,hands = []):
 		self.hands = hands
 
-	def get_winning_hand():
-		pass
+	def get_highest_rank_card_hand(self,hands):
+		maxrank = 0
+		high_hand = ''
+		for hand in hands:
+			if(max(hand.ranks) > maxrank):
+				maxrank = max(hand.ranks)
+		for hand in hands:
+			if max(hand.ranks) == maxrank:
+				high_hand = high_hand + hand.cardlist + ','
+		if high_hand[-1] == ',':
+			high_hand = high_hand[:-1]
+		return high_hand
+
+	def break_tie(self,hands):
+		winning_hand = ''
+		card_type = hands[0].handtype
+		if(card_type in [1]):
+			for hand in hands:
+				winning_hand = winning_hand + hand.cardlist + ','
+		elif(card_type in [2]):
+			winning_hand = self.get_highest_rank_card_hand(hands)
+		elif(card_type == 3):
+			max_four_card = 0
+			max_fifth_card = 0
+			for hand in hands:
+				if hand.info['four_card_rank'] > max_four_card:
+					max_four_card = hand.info['four_card_rank']
+				if hand.info['fifth_card_rank'] > max_fifth_card:
+					max_fifth_card = hand.info['fifth_card_rank']
+			cnt = 0
+			winning_hand = ''
+			for hand in hands:
+				if hand.info['four_card_rank'] == max_four_card:
+					if cnt > 1:
+						break;
+					cnt += 1
+					winning_hand = hand.cardlist		
+			for hand in hands:
+				if hand.info['fifth_card_rank'] == max_fifth_card and hand.info['four_card_rank'] == max_four_card:
+					winning_hand = winning_hand + hand.cardlist + ','
+		if winning_hand[-1] == ',':
+			winning_hand = winning_hand[:-1]
+		return winning_hand
+
+	def get_winning_hand(self):
+		handtypes = []
+		hands_tied = []
+		for hand in self.hands:
+			handtypes.append(hand.handtype)
+		mintype = min(handtypes)
+		for hand in self.hands:
+			if(hand.handtype == mintype):
+				hands_tied.append(hand)
+		if(len(hands_tied) == 1):
+			return hands_tied[0].cardlist
+		else:
+			return self.break_tie(hands_tied)							
 
 class Hand(object):
 	
@@ -51,6 +107,11 @@ class Hand(object):
 		elif(self.is_straight_flush()):
 			return 2
 		elif(self.is_kind(4)):
+			for r in self.ranks:
+				if self.ranks.count(r) == 4: 
+					self.info['four_card_rank'] = r
+				elif self.ranks.count(r) == 1:
+					self.info['fifth_card_rank'] = r	
 			return 3
 		elif(self.is_full_house()):
 			return 4
@@ -69,9 +130,10 @@ class Hand(object):
 
 	def __init__(self,cards = ''):
 		self.cards = []
+		self.info = {}
+		self.cardlist = cards
 		for c in cards.split():
-			self.cards.append(Card(c))
-		
+			self.cards.append(Card(c))		
 		self.ranks = []
 		self.suits = []
 		for c in self.cards:
@@ -131,16 +193,50 @@ def hand_test():
 	assert one_pair.get_hand_type() == 9
 
 def poker_test():
-	h1 = ['AH','4H','2H','3H','5H']
-	h2 = ['JH','4D','7H','6D','5D']
-	h3 = ['TD','TC','TH','7C','7D']
-	h4 = ['2C','3C','4C','5C','6S']
-	h5 = ['AS','4S','5S','3S','2S']
-	
-	hands = [h1,h2,h3,h4,h5]	
-	poker = Poker(hands)	
+	royal_flush = Hand('AS KS QS JS TS')
+	straight_flush = Hand('5S 4S 3S 2S AS')
+	four_of_kind = Hand('AH AS AD AC KH')
+	full_house = Hand('AH AS AD KH KS')
+	flush = Hand('AS TS 2S 5S 6S')
+	straight = Hand('5H 4A 3S 2S AC')
+	three_of_kind = Hand('AH AS AC KH QC')
+	two_pair = Hand('AH AC KH KS QC')
+	one_pair = Hand('AH AS KH QS JD')
+	high_card = Hand('AH KS JD 5H 2D')		
 
-	#assert poker.get_winning_hand() == ''	
+	straight_flush1 = Hand('6S 5S 4S 3S 2S')
+	hands = [straight_flush, straight_flush1]
+	poker = Poker(hands)
+	assert poker.get_highest_rank_card_hand(hands) == '6S 5S 4S 3S 2S'
+
+	royal_flush1 = Hand('AH KH QH JH TH')
+	hands = [royal_flush,royal_flush1]
+	poker = Poker(hands)
+	assert poker.break_tie(hands) == 'AS KS QS JS TS,AH KH QH JH TH'
+
+	straight_flush1 = Hand('6S 5S 4S 3S 2S')
+	hands = [straight_flush, straight_flush1]
+	poker = Poker(hands)
+	assert poker.break_tie(hands) == '6S 5S 4S 3S 2S'
+
+	four_of_kind1 = Hand('AH AS AD AC 3H')
+	hands = [four_of_kind,four_of_kind1]
+	poker = Poker(hands)
+	print poker.break_tie(hands)
+	assert poker.break_tie(hands) == 'AH AS AD AC KH'
+
+	four_of_kind1 = Hand('2H 2S 2D 2C 3H')
+	hands = [four_of_kind,four_of_kind1]
+	poker = Poker(hands)
+	print poker.break_tie(hands)
+	assert poker.break_tie(hands) == 'AH AS AD AC KH'
+
+	
+	
+
+	hands = [full_house,flush,straight]	
+	poker = Poker(hands)	
+	assert poker.get_winning_hand() == 'AH AS AD KH KS'
 	
 if __name__ == "__main__":
 	main()
